0727dc06e880878b19d8c0cf98bd394131a9b393
Gregory.Johnson <3531op@gmail.com>
2020-11-10 09:30:07 -0500

    initial commit

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 // version 0:
 // console.log('\u0007');
+const userInput = process.argv.slice(2)
+const mins = 6000
+
+// setTimeout((() => console.log('3 seconds have passed.')), 3000)
+
+// setTimeout(() => console.log(`${userInput} seconds have passed!`), userInput * 1000)
+
+for (const input of userInput) setTimeout(() => console.log(`${input} seconds have passed!`), mins * 1000)
+
+
+
+
+
+
+
+
